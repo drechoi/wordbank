12c5d1be0081828c97d182d0fde0384c4472e881
andre <andre.cfh@gmail.com>
2020-01-01 22:41:33 +0800

    Debug view

---
+++ test/unit/specs/views/DebugView.spec.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import DebugView from '@/views/DebugView';
+import BootstrapVue from 'bootstrap-vue';
+import { shallowMount } from '@vue/test-utils';
+
+// Install the authentication plugin here
+Vue.use(BootstrapVue);
+
+describe('Home.vue - Home View', () => {
+  const wrapper = shallowMount(DebugView);
+  it('render a list of all router path', () => {
+    wrapper.findAll('li').wrappers.forEach(w => console.log(w.html()));
+    expect(wrapper.findAll('li').length).to.equal(12);
+  });
+});
