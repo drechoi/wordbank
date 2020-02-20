6440ac0ca314392077571d81be566bb60f8bd64f
andre <andre.cfh@gmail.com>
2020-02-20 23:11:55 +0800

    fix history fallback

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 // server.js
 var express = require('express');
 var path = require('path');
-// var serveStatic = require('serve-static');
+var serveStatic = require('serve-static');
 var history = require('connect-history-api-fallback');
 
 // const options = require('./router.options.js');
 
 app = express();
 app.use(history({ verbose: true }));
-// app.use(serveStatic(__dirname + "/dist"));
+app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
