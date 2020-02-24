9867df3acaa525b36577ca2320781072d0725b2d
andre <andre.cfh@gmail.com>
2020-02-24 11:54:32 +0800

    firebase config update

--- src/api/firebase/firebaseConfig.js
+++ src/api/firebase/firebaseConfig.js
@@ -1,40 +1,28 @@
 import firebase from 'firebase';
 import 'firebase/firestore';
 
-// import {store} from '@/store/store';
-
 // firebase init goes here
 var config = process.env.FIREBASE_CONFIG;
 
+console.log('firebase config: ');
+console.log(config);
+
 firebase.initializeApp(config);
+
+console.log('fire base init done');
 
 // // firebase utils
 const db = firebase.firestore();
 const auth = firebase.auth();
-// const currentUser = auth.currentUser;
 
-// // firebase collections
 const usersCollection = db.collection('users');
-// wanna change this to schemes
 const schemesCollection = db.collection('scheme');
 const booksCollection = db.collection('books');
-
-// const postsCollection = db.collection('posts');
-// const commentsCollection = db.collection('comments');
-// const likesCollection = db.collection('likes');
 
 export default {
   db,
   auth,
-  // currentUser
   usersCollection,
   schemesCollection,
   booksCollection,
 };
-//   auth,
-//   currentUser,
-//   usersCollection,
-//   postsCollection,
-//   commentsCollection,
-//   likesCollection
-// };
